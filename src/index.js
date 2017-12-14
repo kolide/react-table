42cf4097b09853ad5dcf23e706893ca6ade515ca
Kyle Knight <kyle@kolide.co>
2017-12-14 09:00:58 -0600

    add class

--- src/index.js
+++ src/index.js
@@ -964,6 +964,7 @@ export default class ReactTable extends Methods(Lifecycle(Component)) {
             className={classnames(
               tableProps.className,
               currentlyResizing ? 'rt-resizing' : '',
+              hasMaxWidth ? 'rt-maxwidth' : '',
             )}
             style={tableProps.style}
             {...tableProps.rest}
